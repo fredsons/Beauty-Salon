5cc85ce7c9e84d75ae9520d47136946a739abb7a
Fredson <85347233+fredsons@users.noreply.github.com>
2021-10-17 03:20:28 -0300

    adiciona funcao do click

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 /*  Abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+//Função para o click
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
